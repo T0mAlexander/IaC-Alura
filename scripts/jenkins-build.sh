8f68e23bfc81c08065eba455fa2b32f170c14b20
Tom Alexander <t0malexander@protonmail.com>
2023-07-05 19:15:41 -0300

    feat: :wrench: created shell script

---
+++ scripts/jenkins-build.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+### TODO - Configs do Jenkins ###
+#todo - Iniciar um job acoplado ao repositório do Github via HTTPS na branch "jenkins-docker"
+#todo - Ativar o Poll SCM inserindo o "* * * * *"
+#todo - Instalar plugins do Docker e Config File Provider
+#todo - Ativar o cloud do Docker
+#todo - Criar .env-dev e .env-prod
+#todo - Inserir variavel .env-dev no Build Environment do Job com target ".env"
+#todo - Deletar workspace antes de executar a build
+
+#? Build Step 1
+echo "-------- Validando a sintaxe do Dockerfile --------"
+cd app
+cat Dockerfile
+docker run --rm -i hadolint/hadolint < Dockerfile
+
+#? Build Dockerfile
+#* Diretório: ./app/
+#* Cloud: docker
+#* Img Docker: django_todolist_image_build
+
+#? Build Step 3
+#!/bin/sh
+
+echo "-------- Subindo o container de teste --------"
+docker run -d -p 82:8000 -v /var/run/mysqld/mysqld.sock:/var/run/mysqld/mysqld.sock \
+-v /var/lib/jenkins/workspace/lista-de-tarefas/app/:/usr/src/app/ --name=lista-tarefas-teste django_todolist_image_build
+
+echo "-------- Localizando .env no container --------"
+docker exec -i lista-tarefas-teste ls -a | grep .env
+
+echo "-------- Testando .env --------"
+docker exec -i lista-tarefas-teste cat .env
+
+echo "-------- Testando a imagem --------"
+docker exec -i lista-tarefas-teste python manage.py test --keep
+exit_code=$?
+
+echo "-------- Encerrando o container antigo --------"
+docker rm -f lista-tarefas-teste
+
+if [ $exit_code -ne 0 ]; then
+  exit 1
+fi
