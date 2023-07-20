4de48e64d8822c8dc988ea91a5ab43e83333c1bf
Tom Alexander <t0malexander@protonmail.com>
2023-07-20 04:25:05 -0300

    build: :hammer: updated resources

--- main.tf
+++ main.tf
@@ -16,22 +16,27 @@ resource "aws_instance" "server" {
   instance_type = "t2.micro"              # Instância do Free Tier da AWS
   key_name      = var.ssh-key
 
-  # user_data = <<-EOF
-  # #!/bin/bash
-  # cd /home/ubuntu
-  # echo "<h1>Ola, Ansible e Terraform</h1>" > index.html
-  # nohup busybox httpd -f -p 8080 &
-  # EOF
-
   root_block_device {
     volume_size           = 30
     volume_type           = "gp2"
     delete_on_termination = true
   }
 
-  vpc_security_group_ids = [ aws_security_group.ssh-access.id ]
+  vpc_security_group_ids = [aws_security_group.ssh-access.id]
 
   tags = {
-    "name" = "Ansible Terraform Python"
+    "name" = "Máquina de Desenvolvimento"
+  }
+}
+
+resource "aws_instance" "prod" {
+  ami           = "ami-0af6e9042ea5a4e3e" # Ubuntu Server 22.04 LTS
+  instance_type = "t2.micro"              # Instância do Free Tier da AWS
+  key_name      = var.ssh-key
+
+  vpc_security_group_ids = [aws_security_group.ssh-access.id]
+
+  tags = {
+    "name" = "Máquina de Produção"
   }
 }
