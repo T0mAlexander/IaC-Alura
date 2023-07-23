e0e7e24c2b4fa4338da3eebb6876e931bdf34ae4
Tom Alexander <t0malexander@protonmail.com>
2023-07-22 23:57:40 -0300

    test: :alembic: performed experiment

--- load-test.go
+++ load-test.go
@@ -1,84 +1,206 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"net/http"
 	"os"
 	"os/signal"
+	"strconv"
+	"strings"
+	"syscall"
 	"time"
 )
 
 var (
-	baseValue, increment, even, odds, totalNumbers int
-	startTime, endTime                             time.Time
-	input                                          string
+	targetURL     string
+	numUsers      int
+	rps           int
+	stopTest      chan struct{}
+	results       chan int
+	successCnt    int
+	failureCnt    int
+	totalTime     int
+	maxResponse   int
+	minResponse   int
+	forceStop     bool
+	testStartTime time.Time
+	testEndTime   time.Time
 )
 
-func main() {
-	// Inserindo valores no terminal
-	fmt.Print("Valor base: ")
-	fmt.Scanln(&baseValue)
-
-	fmt.Print("Incremento: ")
-	fmt.Scanln(&increment)
-
-	// Criando um canal para receber sinais de interrupção (Ctrl+C)
-	interrupt := make(chan os.Signal, 1)
-	signal.Notify(interrupt, os.Interrupt)
-
-	done := make(chan struct{}) // Canal para sinalizar que a função test() foi concluída
-
-	go func() {
-		fmt.Println("Digite 'stop' para encerrar")
-		fmt.Scanln(&input)
-
-		// Se o usuário digitou 'stop', enviar um sinal para o canal de interrupção e também para o canal done
-		if input == "stop" {
-			interrupt <- os.Interrupt
-			done <- struct{}{}
+func makeRequest() {
+	for {
+		if forceStop {
+			return
 		}
-	}()
 
-	// Início da contagem
-	startTime = time.Now()
+		startTime := time.Now()
 
-	// Goroutine para fazer a contagem com o incremento
-	go func() {
-		for {
-			for x := 1; x <= increment; x++ {
-				select {
-				case <-done:
-					return
+		// Faz a solicitação HTTP GET
+		resp, err := http.Get(targetURL)
 
-				default:
-					baseValue++
-					fmt.Printf("valor: %d\n", baseValue)
+		if err != nil {
+			fmt.Printf("Erro ao fazer a solicitação: %s\n", err)
+			results <- 0
+			failureCnt++
+		} else {
+			// Verifica o status da resposta
+			if resp.StatusCode == http.StatusOK {
+				duration := int(time.Since(startTime).Milliseconds())
+				results <- duration
+				successCnt++
+				totalTime += duration
+				if duration > maxResponse {
+					maxResponse = duration
+				}
+				if duration < minResponse || minResponse == 0 {
+					minResponse = duration
+				}
+			} else {
+				results <- 0
+				failureCnt++
+			}
+		}
+	}
+}
 
-					// Verificação de números pares e ímpares
-					totalNumbers++
-					if baseValue%2 == 0 {
-						even++
-					} else {
-						odds++
+func readInput(ch chan<- string) {
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		ch <- scanner.Text()
+	}
+}
+
+func sendRequests() {
+	// Cada usuário envia uma requisição a cada 1 segundo (1000 ms / RPS)
+	interval := time.Duration(1000/rps) * time.Millisecond
+
+	// Contador de requisições enviadas por segundo
+	requestsPerSecond := 0
+	ticker := time.NewTicker(interval)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			requestsPerSecond = 0
+		default:
+			if requestsPerSecond < rps {
+				for i := 0; i < numUsers; i++ {
+					if forceStop {
+						return
 					}
+					results <- 1
+					requestsPerSecond++
 				}
 			}
-
-			// Intervalo de 1 segundo após imprimir os valores do incremento
-			time.Sleep(1 * time.Second)
+		case <-stopTest:
+			return
 		}
-	}()
+	}
+}
 
-	// Aguardar a função test() concluir ou o programa ser interrompido (Ctrl+C)
-	<-interrupt
-	fmt.Println("Encerrando o programa")
+func main() {
+	stopTest = make(chan struct{})
+	results = make(chan int)
+	successCnt = 0
+	failureCnt = 0
+	totalTime = 0
+	maxResponse = 0
+	minResponse = 0
+	forceStop = false
 
-	// Resultados finais do programa
-	fmt.Printf("Total de algarismos: %d\n", totalNumbers)
-	fmt.Printf("Números pares: %d (%.2f%%)\n", even, float64(even)/float64(totalNumbers)*100)
-	fmt.Printf("Números ímpares: %d (%.2f%%)\n", odds, float64(odds)/float64(totalNumbers)*100)
+	fmt.Println("Digite a URL de destino:")
+	scanner := bufio.NewScanner(os.Stdin)
+	if scanner.Scan() {
+		targetURL = scanner.Text()
+	}
 
-	// Registro do término da contagem
-	endTime = time.Now()
-	testDuration := endTime.Sub(startTime)
-	fmt.Printf("Duração do teste: %.1f segundos\n", testDuration.Seconds())
+	fmt.Println("Digite o número de usuários:")
+	if scanner.Scan() {
+		numUsers, _ = strconv.Atoi(scanner.Text())
+	}
+
+	fmt.Println("Digite a taxa de requisições por segundo (RPS):")
+	if scanner.Scan() {
+		rps, _ = strconv.Atoi(scanner.Text())
+	}
+
+	fmt.Println("Iniciando o teste de carga...")
+	testStartTime = time.Now()
+	fmt.Println("Digite 'change url <URL>', 'change users <num>', 'change rps <rps>' ou 'parar' para encerrar o teste e exibir os resultados.")
+
+	for x := 0; x < numUsers; x++ {
+		go makeRequest()
+	}
+
+	go sendRequests()
+
+	inputCh := make(chan string)
+	go readInput(inputCh)
+
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, syscall.SIGINT)
+
+loop:
+	for {
+		select {
+		case input := <-inputCh:
+			args := strings.Fields(input)
+			if len(args) >= 1 {
+				command := args[0]
+				switch command {
+				case "change":
+					if len(args) == 3 {
+						param := args[1]
+						value, err := strconv.Atoi(args[2])
+						if err != nil {
+							fmt.Println("Valor inválido. Use um número inteiro.")
+							continue
+						}
+						switch param {
+						case "url":
+							targetURL = args[2]
+							fmt.Printf("URL de destino alterada para %s\n", targetURL)
+						case "users":
+							numUsers = value
+							fmt.Printf("Número de usuários alterado para %d\n", numUsers)
+						case "rps":
+							rps = value
+							fmt.Printf("Taxa de requisições por segundo (RPS) alterada para %d\n", rps)
+						default:
+							fmt.Println("Comando inválido. Use 'change url <URL>', 'change users <num>', 'change rps <rps>' ou 'parar'.")
+						}
+					} else {
+						fmt.Println("Comando inválido. Use 'change url <URL>', 'change users <num>', 'change rps <rps>' ou 'parar'.")
+					}
+				case "parar":
+					close(stopTest)
+					forceStop = true
+					fmt.Println("Encerrando o teste de carga...")
+					time.Sleep(1 * time.Second) // Aguarda 1 segundo para permitir que as goroutines terminem
+					testEndTime = time.Now()
+					fmt.Println("Teste de carga concluído:")
+					fmt.Printf("Número total de solicitações bem-sucedidas: %d\n", successCnt)
+					fmt.Printf("Número total de falhas: %d\n", failureCnt)
+					fmt.Printf("Tempo médio de resposta: %d ms\n", totalTime/successCnt)
+					fmt.Printf("Tempo de resposta máximo: %d ms\n", maxResponse)
+					fmt.Printf("Tempo de resposta mínimo: %d ms\n", minResponse)
+					fmt.Printf("Tempo total do teste: %.1f segundos\n", testEndTime.Sub(testStartTime).Seconds())
+					break loop
+				default:
+					fmt.Println("Comando inválido. Use 'change url <URL>', 'change users <num>', 'change rps <rps>' ou 'parar'.")
+				}
+			} else {
+				fmt.Println("Comando inválido. Use 'change url <URL>', 'change users <num>', 'change rps <rps>' ou 'parar'.")
+			}
+		case res := <-results:
+			if res > 0 {
+				// Comentamos a exibição das respostas recebidas para evitar excesso de saída no console
+				// fmt.Printf("Resposta recebida em %d ms\n", res)
+			} else {
+				fmt.Println("Erro na solicitação.")
+			}
+		}
+	}
 }
