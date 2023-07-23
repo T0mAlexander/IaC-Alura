b3c8070a3e7a05c4fa5c658a668f1864e3b52bf0
Tom Alexander <t0malexander@protonmail.com>
2023-07-23 04:44:21 -0300

    test: :test_tube: updated failed load test

--- load-test.go
+++ load-test.go
@@ -2,29 +2,91 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 	"os/signal"
 	"time"
 )
 
 var (
-	baseValue, increment, even, odds, totalNumbers int
-	startTime, endTime                             time.Time
+	rps, totalRequests, successfulRequests, failedRequests int
+	startTime, endTime                                     time.Time
+	url                                                    string
+	minResponseTime, maxResponseTime                       time.Duration
 )
 
-func main() {
-	// Inserindo valores no terminal
-	fmt.Print("Valor base: ")
-	fmt.Scanln(&baseValue)
+func requestURL(url string) {
+	response, error := http.Get(url)
 
-	fmt.Print("Incremento: ")
-	fmt.Scanln(&increment)
+	if error != nil {
+		// fmt.Printf("Erro ao fazer requisição: %s\n", error)
+
+		// A cada erro de requisição, é acrescentado uma falha para relatório final
+		failedRequests++
+		return
+	}
+
+	defer response.Body.Close()
+
+	// Cálculo da duração
+	duration := time.Since(startTime)
+
+	// Rastreamento de tempos de resposta mínimo (mínimo e máximo)
+	if minResponseTime == 0 || duration < minResponseTime {
+		minResponseTime = duration
+	}
+
+	if duration > maxResponseTime {
+		maxResponseTime = duration
+	}
+
+	// Acrescenta +1 a cada requisição bem sucedida
+	successfulRequests++
+
+	// Logs das requisições
+	// fmt.Printf("Req: %d, Tempo: %v\n", count, url)
+}
+
+func makeRequests(done <-chan struct{}) {
+	for {
+		select {
+		case <-done:
+			return // Encerra a goroutine caso o canal done seja fechado
+		default:
+			// Envia uma requisição em paralelo
+			for i := 0; i < rps; i++ {
+				go func() {
+					requestURL(url)
+					totalRequests++
+				}()
+			}
+
+			// Intervalo entre as requisições
+			time.Sleep(time.Second * 2)
+		}
+	}
+}
+
+func main() {
+	// Início da contagem
+	startTime = time.Now()
+
+	// Inserindo valores no terminal
+	fmt.Print("URL: ")
+	fmt.Scanln(&url)
+
+	fmt.Print("Requisições por segundo (RPS): ")
+	fmt.Scanln(&rps)
+
+	// Calcular o número de requisições por carga (em vez de por segundo)
+	requestsPerLoad := rps
 
 	// Criando um canal para receber sinais de interrupção (Ctrl+C)
 	interrupt := make(chan os.Signal, 1)
 	signal.Notify(interrupt, os.Interrupt)
 
-	done := make(chan struct{}) // Canal para sinalizar que a função test() foi concluída
+	done := make(chan struct{}) // Canal para sinalizar que a função foi concluída
+	go makeRequests(done)
 
 	go func() {
 		var input string
@@ -38,47 +100,34 @@ func main() {
 		}
 	}()
 
-	// Início da contagem
-	startTime = time.Now()
-
-	// Goroutine para fazer a contagem com o incremento
+	requestCount := 0
 	go func() {
 		for {
-			for x := 1; x <= increment; x++ {
-				select {
-				case <-done:
-					return
-
-				default:
-					baseValue++
-					fmt.Printf("valor: %d\n", baseValue)
-
-					// Verificação de números pares e ímpares
-					totalNumbers++
-					if baseValue%2 == 0 {
-						even++
-					} else {
-						odds++
-					}
-				}
+			for x := 0; x < requestsPerLoad; x++ {
+				requestURL(url)
+				totalRequests++
 			}
 
-			// Intervalo de 1 segundo após imprimir os valores do incremento
-			time.Sleep(1 * time.Second)
+			requestCount++
+
+			// Intervalo de tempo entre as cargas de requisições
+			time.Sleep(time.Second * 2)
 		}
 	}()
 
-	// Aguardar a função test() concluir ou o programa ser interrompido (Ctrl+C)
+	// Aguardar a função concluir ou o programa ser interrompido (Ctrl+C)
 	<-interrupt
 	fmt.Println("Encerrando o programa")
 
-	// Resultados finais do programa
-	fmt.Printf("Total de algarismos: %d\n", totalNumbers)
-	fmt.Printf("Números pares: %d (%.2f%%)\n", even, float64(even)/float64(totalNumbers)*100)
-	fmt.Printf("Números ímpares: %d (%.2f%%)\n", odds, float64(odds)/float64(totalNumbers)*100)
-
 	// Registro do término da contagem
 	endTime = time.Now()
-	testDuration := endTime.Sub(startTime)
-	fmt.Printf("Duração do teste: %.1f segundos\n", testDuration.Seconds())
+
+	// Resultados finais do programa
+	totalRequests = successfulRequests + failedRequests
+	fmt.Printf("Total de requisições: %d\n", totalRequests)
+	fmt.Printf("Requisições bem sucedidas: %d (%.2f%%)\n", successfulRequests, float64(successfulRequests)/float64(totalRequests)*100)
+	fmt.Printf("Requisições falhadas: %d (%.2f%%)\n", failedRequests, float64(failedRequests)/float64(totalRequests)*100)
+
+	testDuration := endTime.Sub(startTime).Seconds()
+	fmt.Printf("Duração do teste: %.1f segundos\n", testDuration)
 }
